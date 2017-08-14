7f56a80b8b39a4f36f8b63ab764a0948d61897b3
SageStrieker <sspink157@gmail.com>
2017-08-14 10:24:08 -0700

    fixed login handlers

--- main.py
+++ main.py
@@ -33,13 +33,15 @@ class MainHandler(webapp2.RequestHandler):
     	template=env.get_template('login.html')
         user = users.get_current_user()
         if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                (user.nickname(), users.create_logout_url('/profile')))
+            usernickname = user.nickname() 
+            redirect = users.create_logout_url('/')
+            self.response.write(template.render({'user': True, 'usernickname':usernickname, 'redirect': redirect}))
         else:
-            greeting = ('<a href="%s">Sign in or register</a>.' %
-                (users.create_login_url('/profile')))
-        
-        self.response.write('<html><body>%s</body></html>' % greeting)
+            create_login = users.create_login_url('/profile')
+            user.put()
+            self.response.write(template.render({'user': False, 'create_login':create_login}))
+            self.response.write(create_login)
+
         #templates:
             #gotta figure out how to transcribe this into html/jinja cuz right now it just ugly and bad 
             #need an href that says aboutme 
@@ -58,9 +60,9 @@ class AwardsHandler(webapp2.RequestHandler):
         #templates:
             #need just have a blurb about the goals etc in paragraph style
 
-class WorkoutHistoryHandler(webapp2.RequestHandler):
+class WorkoutHandler(webapp2.RequestHandler):
     def get(self):
-        template=env.get_template('workout_history.html')
+        template=env.get_template('workout.html')
         self.response.write(template.render())
         #templates:
             #need just have a blurb about the goals etc in paragraph style
@@ -72,7 +74,7 @@ class RoutineHandler(webapp2.RequestHandler):
         #templates:
             #need just have a blurb about the goals etc in paragraph style
 
-class WorkoutsHistoryHandle(webapp2.RequestHandler):
+class WorkoutsHistoryHandler(webapp2.RequestHandler):
     def get(self):
         template=env.get_template('workout_history.html')
         self.response.write(template.render())
@@ -84,40 +86,40 @@ class ProfileHandler(webapp2.RequestHandler):
     	template=env.get_template('main.html')
         self.response.write(template.render())
 
-   #user
-   def post(self):
-        template=env.get_template('main.html')
-        user = User(name=self.request.get('name'), int(workout_awards=self.request.get('height')), int(weight=self.request.get('weight')))
-        user.put()
-        self.response.write(template.render({'user':user}))
-        #need to probably do this at login or from profile or something 
-        #need corresponding jinja
+   # #user
+   # def post(self):
+   #      template=env.get_template('main.html')
+   #      user = User(name=self.request.get('name'), int(height=self.request.get('height')), int(weight=self.request.get('weight')), skill_level=self.request.get('skill_level'))
+   #      user.put()
+   #      self.response.write(template.render({'user':user}))
+   #      #need to probably do this at login or from profile or something 
+   #      #need corresponding jinja
 
-   #awards
-   def post(self):
-        template=env.get_template('main.html')
-        awards = Awards(login_awards=self.request.get('login_awards'), workout_awards=self.request.get('workout_awards'))
-        awards.put()
-        self.response.write(template.render({'awards':awards}))
-        #might need to make a new template for this thing that might be the easiest that is rendered after clicking a link a side div with the dropdown menu thing
-        #need corresponding jinja 
+   # #awards
+   # def post(self):
+   #      template=env.get_template('awards.html')
+   #      awards = Awards(login_awards=self.request.get('login_awards'), workout_awards=self.request.get('workout_awards'))
+   #      awards.put()
+   #      self.response.write(template.render({'awards':awards}))
+   #      #might need to make a new template for this thing that might be the easiest that is rendered after clicking a link a side div with the dropdown menu thing
+   #      #need corresponding jinja 
 
-   #profile 
-   def post(self):
-        template=env.get_template('main.html')
-        user = User.query(User.name == name).get()
-        awards = Awards.query(User.name == name).get()
-        profile = Profile(int(workout_data=self.request.get('workout_data')), routine=self.request.get('routine'), user_key=self.request.get(user.key), awards_key=self.request.get(awards.key))
-        profile.put()
-        self.response.write(template.render({'awards':awards}))
-        #might need to make a new template for this thing that might be the easiest that is rendered after clicking a link a side div with the dropdown menu thing
-        #need corresponding jinja         
-    #templates: 
-        #need new user button that pulls up a form if you click on it that allows you to implement the user properties with jinja 
-        #need tabs with workouts as an option and awards 
-        #need separate html that has all of the awards information on it 
+   # #profile 
+   # def post(self):
+   #      template=env.get_template('main.html')
+   #      user = User.query(User.name == name).get()
+   #      awards = Awards.query(User.name == name).get()
+   #      profile = Profile(int(workout_data=self.request.get('workout_data')), routine=self.request.get('routine'), user_key=self.request.get(user.key), awards_key=self.request.get(awards.key))
+   #      profile.put()
+   #      self.response.write(template.render({'profile':profile}))
+   #      #might need to make a new template for this thing that might be the easiest that is rendered after clicking a link a side div with the dropdown menu thing
+   #      #need corresponding jinja         
+   #  #templates: 
+   #      #need new user button that pulls up a form if you click on it that allows you to implement the user properties with jinja 
+   #      #need tabs with workouts as an option and awards 
+   #      #need separate html that has all of the awards information on it 
 
-    #MIGHT NEED A WORKOUT_DATA HANDLER THAT LINKS TO THE FORM ON THE WORKOUT_HISTORY PAGE 
+   #  #MIGHT NEED A WORKOUT_DATA HANDLER THAT LINKS TO THE FORM ON THE WORKOUT_HISTORY PAGE 
 
 
 app = webapp2.WSGIApplication([
@@ -125,9 +127,9 @@ app = webapp2.WSGIApplication([
     ('/about', AboutHandler),
     # the above can be a login or /login can be the login, either way
     ('/awards', AwardsHandler),
-    ('/workout_history', WorkoutHistoryHandler)
-    ('/routine', RoutineHandler)
-    ('/workouts', WorkoutsHandler)
+    ('/workout_history', WorkoutsHistoryHandler),
+    ('/routine', RoutineHandler),
+    ('/workouts', WorkoutHandler),
     ('/profile', ProfileHandler)
     #do awards last we need to think about this one and might be easier if not associated with awards, we can just link it to another page rather than having to deal with it all being on one page
     # maybe can just make a link to a separate file
--- models.py
+++ models.py
@@ -1,18 +1,21 @@
 from google.appengine.ext import ndb
 
 class User(ndb.Model):
-   	name = ndb.StringProperty()
-    height = ndb.IntegerProperty()
-    weight = ndb.IntegerProperty()
-    skill_level = ndb.StringProperty()
+  name = ndb.StringProperty()
+  height = ndb.IntegerProperty()
+  weight = ndb.IntegerProperty()
+  skill_level = ndb.StringProperty()
+  timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
 
 class Awards(ndb.Model):
-	login_awards = ndb.IntegerProperty()
-	workout_awards = ndb.StringProperty()
+  login_awards = ndb.IntegerProperty()
+  workout_awards = ndb.StringProperty()
+  timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
 
 class Profile(ndb.Model):
-   	 workout_data = ndb.StringProperty()
-     routine = ndb.StringProperty()
-     # awards = ndb.StringProperty()
-     user_key = ndb.KeyProperty()
-     awards_key = ndb.KeyProperty()
+  workout_data = ndb.StringProperty()
+  # awards = ndb.StringProperty()
+  user_key = ndb.KeyProperty()
+  awards_key = ndb.KeyProperty()
