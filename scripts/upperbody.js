537f9b8110318bf5e8f39b55ab98738bdd6a0ad8
Lauren Lindsey <laurenlindsey@Laurens-MacBook-Pro.local>
2017-08-15 15:20:01 -0700

    table appears when button is clicked

--- scripts/upperbody.js
+++ scripts/upperbody.js
@@ -1,13 +1,30 @@
-function setupHandlers() {
-  $('.1').on('click', display1);
-  $('.2').on('click', display2);
-  $('.3').on('click', display3);
-  $('.4').on('click', display4);
-}
-$(document).ready(setupHandlers);
 
 function display1() {
-  $('.welcome').text("table 1!!");
+  $('.welcome').html(`  <table>
+      <tr>
+          <th colspan="2">Upper Body Sample Routine #1</th>
+      </tr>
+      <tr>
+        <span class="bolded">  <td>Exercise</td>
+          <td>Reps</td> </span>
+      </tr>
+      <tr>
+          <td>Push Ups</td>
+          <td>10</td>
+      </tr>
+      <tr>
+          <td>Bench Dips</td>
+          <td>12</td>
+      </tr>
+      <tr>
+          <td>Chin Ups</td>
+          <td>10</td>
+      </tr>
+      <tr>
+          <td>Plank</td>
+          <td>30 (sec)</td>
+      </tr>
+    </table>`);
   $('.welcome').animate({opacity:1}, 1000)
 }
 function display2() {
@@ -22,3 +39,12 @@ function display4() {
   $('.welcome').text("table 4!!");
   $('.welcome').animate({opacity:1}, 1000)
 }
+
+
+function setupHandlers() {
+  $('.1').on('click', display1);
+  $('.2').on('click', display2);
+  $('.3').on('click', display3);
+  $('.4').on('click', display4);
+}
+$(document).ready(setupHandlers);
